7689be8843eba3ec9ad2b4c4481c6c482bef4cc1
breakpoint <30924539+2281123066@users.noreply.github.com>
2020-03-21 15:57:28 +0800

    Update yolo3_test.py

--- yolo3_test.py
+++ yolo3_test.py
@@ -13,7 +13,7 @@ def detect_image_test(img_path, out_path):
         r_image = yolo.detect_image(src_img)
         yolo.close_session()
 
-        r_image.save(os.path.join(out_path, '1.png'))
+        r_image.save(out_path)
 
 
 if __name__ == '__main__':
@@ -21,7 +21,7 @@ if __name__ == '__main__':
     out_path = './output/'
     img_path = './test/1.jpg'
     video_path = './test/test4.mp4'
-    detect_image_test(img_path, out_path) # 单张图片测试
+    detect_image_test(img_path, os.path.join(out_path, "1.png")) # 单张图片测试
 
     # detect_video(video_path, output_path=os.path.join(out_path, 'out_test4.mp4')) # 本地视频测试
     #  detect_video(video_path=None, output_path=""))  # 实时视频测试
